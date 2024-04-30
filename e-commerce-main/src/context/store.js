997896fb8a463d91ad5fa9c9504583cf973f3211
ibrohim <https://github.com/IbrokhimShukurullayev>
2024-04-30 23:24:48 +0500

    ibrohim

--- e-commerce-main/src/context/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import wishlistSlice from "./wishlistSlice";
-import cartSlice from "./cartSlice";
-export const store = configureStore({
-  reducer: {
-    wishlist: wishlistSlice,
-    cart: cartSlice,
-  },
-});
--- src/pages/siginUp/SignUp.js
+++ src/pages/siginUp/SignUp.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function SignUp() {
-  return <div>SignUp</div>;
+  return <div>ibrohim</div>;
 }
 
 export default SignUp;
